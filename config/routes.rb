739887b08b5b9e3a502df4d89319e9060810bc09
misatol8k <misatolkurihara@gmail.com>
2020-12-25 23:39:19 +0900

    ルーティングを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'sessions/new'
   get 'pictures/index'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :favorites, only: [:create, :destroy]
+  resources :pictures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
