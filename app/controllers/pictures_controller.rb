e0b84ea66f269f6b363debb1492ea679ed1ac16b
misatol8k <misatolkurihara@gmail.com>
2020-12-25 23:54:16 +0900

    pictures_controllerのCRUD機能を実装しました

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,60 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
   def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = current_user.pictures.build
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "投稿しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  def update
+    @picture = current_user.pictures.build(picture_params)
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "更新しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # 書き方エラー出ないか確認する
+    @picture.destroy
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: '削除しました！' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+  def picture_params
+    params.require(:picture).permit(:title, :content, :image, :image_cache)
   end
 end
