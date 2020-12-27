365668d2a3858c2e897ce87ead3f654a473bdb8b
misatol8k <misatolkurihara@gmail.com>
2020-12-27 19:18:25 +0900

    投稿確認のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :favorites, only: [:create, :destroy]
-  resources :pictures
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
