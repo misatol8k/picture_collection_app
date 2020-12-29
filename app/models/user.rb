044d99484945868ba2d4afbd97710f4885ede689
misatol8k <misatolkurihara@gmail.com>
2020-12-30 00:07:09 +0900

    userからお気に入りのpictureを取得するアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :pictures, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
