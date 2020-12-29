0249cc52ae843a0ca88f99e07e7379bc2115eac9
misatol8k <misatolkurihara@gmail.com>
2020-12-30 00:06:54 +0900

    userからお気に入りのpictureを取得するアソシエーションを設定

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,2 +1,7 @@
 class PostMailer < ApplicationMailer
+  def post_mail(current_user, picture)
+    @current_user = current_user
+    @picture = picture
+    mail to: @current_user.email , subject: "投稿確認メール"
+  end
 end
