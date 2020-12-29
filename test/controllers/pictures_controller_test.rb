2e275249790ad63ab981075eaf1b0a02f581e779
misatol8k <misatolkurihara@gmail.com>
2020-12-30 00:04:12 +0900

    テストを記載

--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -6,4 +6,24 @@ class PicturesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get new" do
+    get pictures_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get pictures_show_url
+    assert_response :success
+  end
+  #
+  # test "should get " do
+  #   get pictures_edit_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get " do
+  #   get pictures_edit_url
+  #   assert_response :success
+  # end
+
 end
