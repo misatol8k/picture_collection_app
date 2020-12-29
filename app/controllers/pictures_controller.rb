a2fd9a997fa1382b4b12ded04e4c002d4bc0309d
misatol8k <misatolkurihara@gmail.com>
2020-12-29 11:55:32 +0900

    投稿確認メールの送信機能を追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -22,6 +22,7 @@ class PicturesController < ApplicationController
       render :new
     else
       if @picture.save
+        PostMailer.post_mail(@current_user).deliver 
         redirect_to pictures_path, notice: "投稿しました！"
       else
         render :new
