c96414c33fbfda016ba9af817ced2fc716676a1c
misatol8k <misatolkurihara@gmail.com>
2020-12-30 00:03:15 +0900

    お気に入り一覧表示、登録、削除機能を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,16 @@
 class FavoritesController < ApplicationController
+  def show
+    @favorites = current_user.favorite_pictures
+  end
+
   def create
-    favorite = current_user.favorites.create(picture_id: params[:picture_id])
-    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの記事をお気に入り登録しました"
+    @favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_url, notice: "#{@favorite.picture.user.name}さんの記事をお気に入り登録しました"
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの記事をお気に入り解除しました"
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to pictures_url, notice: "#{@favorite.picture.user.name}さんの記事をお気に入り解除しました"
   end
 
-  def show
-  end
-  
 end
